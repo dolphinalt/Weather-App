6f33d7f50e592aff2d93d01a9331aed0edd5f029
Dolphin <ethan.zhao.us@gmail.com>
2022-10-22 17:33:52 -0700

    baseline

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const readline = require('readline');
 
-async function processLineByLine() {
+async function Process() {
     const fileStream = fs.createReadStream('./assets/data.toml');
 
     const rl = readline.createInterface({
@@ -11,12 +11,29 @@ async function processLineByLine() {
 
     for await (const line of rl) {
         if (/\[\[[aA-zZ]*\]\]/i.test(line)) {
-            console.log(line);
-        } else if (/\[\[[0-9]*\]\]/i.test(line)) {
-            console.log(line);
+            lineRes=line.split("[[").pop();
+            location = lineRes.split("]]").shift()
+            document.getElementById("location").innerHTML = location;
+        } else if (/\[\[[0-9.]*\]\]/i.test(line)) {
+            lineRes=line.split("[[").pop();
+            out=lineRes.replace("/./g", "/");
+            date = out.split("]]").shift()
+            document.getElementById("date").innerHTML = date;
         } else if (/temperature = /i.test(line)) {
-            console.log(line);
+            temperature = line.split(" = ").pop()
+            document.getElementById("temperature").innerHTML = temperature;
+        } else if (/low = /i.test(line)) {
+            lowTemperature = line.split(" = ").pop()
+            document.getElementById("lowTemperature").innerHTML = lowTemperature;
+        } else if (/high = /i.test(line)) {
+            highTemperature = line.split(" = ").pop()
+            document.getElementById("highTemperature").innerHTML = highTemperature;
+        } else if (/quality = /i.test(line)) {
+            lineRes=line.replace(".", "/");
+            quality = line.split(" = ").pop()
+            document.getElementById("quality").innerHTML = quality;
+        }
     }
 }
 
-processLineByLine();
+Process();
