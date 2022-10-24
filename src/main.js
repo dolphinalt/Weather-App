b84c603cbdf11b5ca059660286643155c8ce9675
Dolphin <ethan.zhao.us@gmail.com>
2022-10-24 15:12:46 -0700

    lmfao

--- src/main.js
+++ src/main.js
@@ -1,40 +1,35 @@
+function main() {
 const $output = document.getElementById('./assets/data.toml')
-document.getElementById('file').onchange = function() {
-  var file = this.files[0];
+document.getElementById('./assets/data.toml').onchange = function() {
+    var file = this.files[0];
 
-  var reader = new FileReader();
-  reader.onload = function(progressEvent) {
-    // Entire file
-    const text = this.result;
-    $output.innerText = text
+    var reader = new FileReader();
+    reader.onload = function(progressEvent) {
+        const text = this.result;
+        $output.innerText = text
 
-    // By lines
-    var lines = text.split('\n');
-    for (var line = 0; line < lines.length; line++) {
-      console.log(lines[line]);
-    }
-  };
-  reader.readAsText(file);
-};
-    for await (const line of rl) {
-        if (/\[\[[aA-zZ]*\]\]/i.test(line)) {
-            lineRes=line.split("[[").pop();
-            location = lineRes.split("]]").shift()
-        } else if (/\[\[[0-9.]*\]\]/i.test(line)) {
-            lineRes=line.split("[[").pop();
-            out=lineRes.replace("/./g", "/");
-            date = out.split("]]").shift()
-        } else if (/temperature = /i.test(line)) {
-            temperature = line.split(" = ").pop()
-        } else if (/low = /i.test(line)) {
-            lowTemperature = line.split(" = ").pop()
-        } else if (/high = /i.test(line)) {
-            highTemperature = line.split(" = ").pop()
-        } else if (/quality = /i.test(line)) {
-            lineRes=line.replace(".", "/");
-            quality = line.split(" = ").pop()
+        var lines = text.split('\n');
+        for (line of lines) {
+            if (/\[\[[aA-zZ]*\]\]/i.test(line)) {
+                lineRes=line.split("[[").pop();
+                location = lineRes.split("]]").shift()
+            } else if (/\[\[[0-9.]*\]\]/i.test(line)) {
+                lineRes=line.split("[[").pop();
+                out=lineRes.replace("/./g", "/");
+                date = out.split("]]").shift()
+            } else if (/temperature = /i.test(line)) {
+                temperature = line.split(" = ").pop()
+            } else if (/low = /i.test(line)) {
+                lowTemperature = line.split(" = ").pop()
+            } else if (/high = /i.test(line)) {
+                highTemperature = line.split(" = ").pop()
+            } else if (/quality = /i.test(line)) {
+                lineRes=line.replace(".", "/");
+                quality = line.split(" = ").pop()
+            }
         }
-    }
-}
+    };
+    reader.readAsText(file);
+};
 
-Process();
+}
