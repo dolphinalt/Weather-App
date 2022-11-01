584a185040638a6396c4c1671e44df2d4c6df2f6
Dolphin <ethan.zhao.us@gmail.com>
2022-11-01 15:08:54 -0700

    deploy api

--- src/test.js
+++ src/test.js
@@ -1,7 +1,14 @@
-class HelloMessage extends React.Component {
-    render() {
-        return <div>Hello {this.props.name}</div>;
-    }
-}
-  
-root.render(<HelloMessage name="Taylor" />);
+const data = null;
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
+});
+
+xhr.open("GET", "http://127.0.0.1:5000/weatherData/");
+
+xhr.send(data);
