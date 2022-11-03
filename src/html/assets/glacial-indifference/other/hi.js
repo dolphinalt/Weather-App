5ed00c2667bca7dc844abec2ec3d84750c372f3b
Dolphin <ethan.zhao.us@gmail.com>
2022-11-03 00:28:45 -0700

    release v1.0

---
+++ src/html/assets/glacial-indifference/other/hi.js
@@ -0,0 +1,6 @@
+function help() {
+    console.log("Help i did like 99% of the work on this")
+    console.log("also i think this counts as an easter egg")
+    console.log("and @dolphinalt and @dolphincyber are the same person btw")
+    console.log("i think u can guess who i am")
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 var userWants = "San,Diego,USA"
 
 function main(wantedLocation) {
+    console.log("[i] Hello! Welcome to the console. For those who are here for debugging, you can ignore the SyntaxErrors, as the webpage runs perfectly fine with those there.")
+    console.log("[!] Credits: @dolphinalt, @dolphincyber, @Orlando-c, @raisinbran25, @BobTheFarmer on github")
     const data = null;
     var subpage = "weatherDataSD"
     
@@ -13,7 +15,7 @@ function main(wantedLocation) {
     
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === this.DONE) {
-            console.log(this.responseText);
+            console.log("[i] dataRecieved");
         }
         const weatherData = JSON.parse(this.responseText);
         // console.log(weatherData.columns)
@@ -38,7 +40,6 @@ function main(wantedLocation) {
             var locationDisplay = locationSplit[0];
         }
         var locationDisplayFinal = locationDisplay.toUpperCase()
-        var date0 = weatherData["locations"][place]["values"][0]["datetimeStr"]
         var temp0 = weatherData["locations"][place]["values"][0]["temp"]
         var temp0Res = temp0.toPrecision(2)
         var maxTemp0 = weatherData["locations"][place]["values"][0]["maxt"]
@@ -46,35 +47,110 @@ function main(wantedLocation) {
         var lowHigh0 = "High: " + maxTemp0.toPrecision(2) + "° Low: " + lowTemp0.toPrecision(2) + "°"
         var conditions0 = weatherData["locations"][place]["values"][0]["conditions"]
         // day 2
-        var date1 = weatherData["locations"][place]["values"][1]["datetimeStr"]
         var temp1 = weatherData["locations"][place]["values"][1]["temp"]
-        var temp1Res = temp0.toPrecision(2)
+        var temp1Res = temp1.toPrecision(2)
         var maxTemp1 = weatherData["locations"][place]["values"][1]["maxt"]
         var lowTemp1 = weatherData["locations"][place]["values"][1]["mint"]
         var lowHigh1 = "High: " + maxTemp1.toPrecision(2) + "° Low: " + lowTemp1.toPrecision(2) + "°"
         var conditions1 = weatherData["locations"][place]["values"][1]["conditions"]
         // day 3
         var date2 = weatherData["locations"][place]["values"][2]["datetimeStr"]
+        var date2res = date2.slice(5,10)
+        var date2final = date2res.replace("-", "/")
         var temp2 = weatherData["locations"][place]["values"][2]["temp"]
-        var temp2Res = temp0.toPrecision(2)
+        var temp2Res = temp2.toPrecision(2)
         var maxTemp2 = weatherData["locations"][place]["values"][2]["maxt"]
         var lowTemp2 = weatherData["locations"][place]["values"][2]["mint"]
         var lowHigh2 = "High: " + maxTemp2.toPrecision(2) + "° Low: " + lowTemp2.toPrecision(2) + "°"
         var conditions2 = weatherData["locations"][place]["values"][2]["conditions"]
         //day 4
         var date3 = weatherData["locations"][place]["values"][3]["datetimeStr"]
+        var date3res = date3.slice(5,10)
+        var date3final = date3res.replace("-", "/")
         var temp3 = weatherData["locations"][place]["values"][3]["temp"]
-        var temp3Res = temp0.toPrecision(2)
+        var temp3Res = temp3.toPrecision(2)
         var maxTemp3 = weatherData["locations"][place]["values"][3]["maxt"]
         var lowTemp3 = weatherData["locations"][place]["values"][3]["mint"]
         var lowHigh3 = "High: " + maxTemp3.toPrecision(2) + "° Low: " + lowTemp3.toPrecision(2) + "°"
         var conditions3 = weatherData["locations"][place]["values"][3]["conditions"]
     
         // set vars
+        // today
         document.getElementById('place').textContent = locationDisplayFinal;
         document.getElementById('todayTemperature').textContent = temp0Res;
-        document.getElementById('quality').textContent = conditions0;
-        document.getElementById('lowHigh').textContent = lowHigh0;
+        document.getElementById('todayQuality').textContent = conditions0;
+        document.getElementById('todayLowHigh').textContent = lowHigh0;
+        // day 2
+        document.getElementById('tile0temp').textContent = temp1Res;
+        document.getElementById('tile0quality').textContent = conditions1;
+        if (conditions1.includes("Rain")) {
+            svg='<img src="./assets/SVG/Rainy.svg" alt="' + conditions1 + '" height="100" width="100" style="display: block;margin: auto;margin-top: -20px;"/>'
+            document.getElementById('tile0glyph').innerHTML = svg;
+        } else if (conditions1.includes("Clear")) {
+            svg='<img src="./assets/SVG/Sunny.svg" alt="' + conditions1 + '" height="100" width="100" style="display: block;margin: auto;margin-top: -20px;"/>'
+            document.getElementById('tile0glyph').innerHTML = svg;
+        } else if (conditions1.includes("Partially cloudy")) {
+            svg='<img src="./assets/SVG/PartiallyCloudy.svg" alt="' + conditions1 + '" height="100" width="100" style="display: block;margin: auto;margin-top: -20px;"/>'
+            document.getElementById('tile0glyph').innerHTML = svg;
+        } else if (conditions1.includes("Mostly cloudy")) {
+            svg='<img src="./assets/SVG/MostlyCloudy.svg" alt="' + conditions1 + '" height="100" width="100" style="display: block;margin: auto;margin-top: -20px;"/>'
+            document.getElementById('tile0glyph').innerHTML = svg;
+        } else if (conditions1.includes("Overcast")) {
+            svg='<img src="./assets/SVG/Cloudy.svg" alt="' + conditions1 + '" height="100" width="100" style="display: block;margin: auto;margin-top: -20px;"/>'
+            document.getElementById('tile0glyph').innerHTML = svg;
+        } else if (conditions1.includes("Storm")) {
+            svg='<img src="./assets/SVG/Stormy.svg" alt="' + conditions1 + '" height="100" width="100" style="display: block;margin: auto;margin-top: -20px;"/>'
+            document.getElementById('tile0glyph').innerHTML = svg;
+        }
+        document.getElementById('tile0lowHigh').textContent = lowHigh1;
+        // day 3
+        document.getElementById('tile1day').textContent = date2final;
+        document.getElementById('tile1temp').textContent = temp2Res;
+        document.getElementById('tile1quality').textContent = conditions2;
+        if (conditions2.includes("Rain")) {
+            svg='<img src="./assets/SVG/Rainy.svg" alt="' + conditions2 + '" height="100" width="100" style="display: block;margin: auto;margin-top: -20px;"/>'
+            document.getElementById('tile1glyph').innerHTML = svg;
+        } else if (conditions2.includes("Clear")) {
+            svg='<img src="./assets/SVG/Sunny.svg" alt="' + conditions2 + '" height="100" width="100" style="display: block;margin: auto;margin-top: -20px;"/>'
+            document.getElementById('tile1glyph').innerHTML = svg;
+        } else if (conditions2.includes("Partially cloudy")) {
+            svg='<img src="./assets/SVG/PartiallyCloudy.svg" alt="' + conditions2 + '" height="100" width="100" style="display: block;margin: auto;margin-top: -20px;"/>'
+            document.getElementById('tile1glyph').innerHTML = svg;
+        } else if (conditions2.includes("Mostly cloudy")) {
+            svg='<img src="./assets/SVG/MostlyCloudy.svg" alt="' + conditions2 + '" height="100" width="100" style="display: block;margin: auto;margin-top: -20px;"/>'
+            document.getElementById('tile1glyph').innerHTML = svg;
+        } else if (conditions2.includes("Overcast")) {
+            svg='<img src="./assets/SVG/Cloudy.svg" alt="' + conditions2 + '" height="100" width="100" style="display: block;margin: auto;margin-top: -20px;"/>'
+            document.getElementById('tile1glyph').innerHTML = svg;
+        } else if (conditions2.includes("Storm")) {
+            svg='<img src="./assets/SVG/Stormy.svg" alt="' + conditions2 + '" height="100" width="100" style="display: block;margin: auto;margin-top: -20px;"/>'
+            document.getElementById('tile1glyph').innerHTML = svg;
+        }
+        document.getElementById('tile1lowHigh').textContent = lowHigh2;
+        // day 4
+        document.getElementById('tile2day').textContent = date3final;
+        document.getElementById('tile2temp').textContent = temp3Res;
+        document.getElementById('tile2quality').textContent = conditions3;
+        if (conditions3.includes("Rain")) {
+            svg='<img src="./assets/SVG/Rainy.svg" alt="' + conditions3 + '" height="100" width="100" style="display: block;margin: auto;margin-top: -20px;"/>'
+            document.getElementById('tile2glyph').innerHTML = svg;
+        } else if (conditions3.includes("Clear")) {
+            svg='<img src="./assets/SVG/Sunny.svg" alt="' + conditions3 + '" height="100" width="100" style="display: block;margin: auto;margin-top: -20px;"/>'
+            document.getElementById('tile2glyph').innerHTML = svg;
+        } else if (conditions3.includes("Partially cloudy")) {
+            svg='<img src="./assets/SVG/PartiallyCloudy.svg" alt="' + conditions3 + '" height="100" width="100" style="display: block;margin: auto;margin-top: -20px;"/>'
+            document.getElementById('tile2glyph').innerHTML = svg;
+        } else if (conditions3.includes("Mostly cloudy")) {
+            svg='<img src="./assets/SVG/MostlyCloudy.svg" alt="' + conditions3 + '" height="100" width="100" style="display: block;margin: auto;margin-top: -20px;"/>'
+            document.getElementById('tile2glyph').innerHTML = svg;
+        } else if (conditions3.includes("Overcast")) {
+            svg='<img src="./assets/SVG/Cloudy.svg" alt="' + conditions3 + '" height="100" width="100" style="display: block;margin: auto;margin-top: -20px;"/>'
+            document.getElementById('tile2glyph').innerHTML = svg;
+        } else if (conditions3.includes("Storm")) {
+            svg='<img src="./assets/SVG/Stormy.svg" alt="' + conditions3 + '" height="100" width="100" style="display: block;margin: auto;margin-top: -20px;"/>'
+            document.getElementById('tile2glyph').innerHTML = svg;
+        }
+        document.getElementById('tile2lowHigh').textContent = lowHigh3;
     });
 
     if (wantedLocation == "San,Diego,USA") {
@@ -91,3 +167,5 @@ function main(wantedLocation) {
     xhr.open("GET", link);
     xhr.send(data);
 }
+
+//<img src="./assets/SVG/Rainy.svg" alt="triangle with all three sides equal" height="100" width="100" style="display: block;margin: auto;margin-top: -20px;"/>
